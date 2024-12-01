8060ce075e8d9b657570dd2d7e1c25e82271bf40
Gia Bao <thainguyengiabao27092004@gmail.com>
2024-12-02 01:07:39 +0700

    add statistic page

---
+++ src/pages/Statictis.page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ChildrenCostsPage from '../components/ChildrenCost.page'
+import DoctorScheduleComponent from '../components/DoctorScheduler.page'
+const Statictis = () => {
+  return (
+    <div>
+      <ChildrenCostsPage />
+      <DoctorScheduleComponent />
+    </div>
+  )
+}
+
+export default Statictis
